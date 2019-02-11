8e2f2d18bd554cb13b3e79eb8d8e97ff8080d3b9
saravana <saravanasaru14313@gmail.com>
2019-02-11 14:29:25 +0530

    Update and rename README.md to README.java

---
+++ README.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+class Sumofnatural
+{
+  public static void main(String args[])
+  {
+	Scanner input=new Scanner(System.in);
+	String a=input.nextLine();
+	for(int i=0;i<a.length();i++)
+	{
+		System.out.print(a.charAt(i));
+		System.out.print(" ");
+	}
+		
+   }
+}	

94d7c854bf99b4739e036835f107dc13c44626a3
saravana <saravanasaru14313@gmail.com>
2019-02-11 07:22:55 -0800

    Update README.java

--- README.java
+++ README.java
@@ -7,7 +7,7 @@ class Printdigits
         int a;
         Scanner input = new Scanner(System.in);
         a = input.nextInt();
-        String b=String.valueOf(c);
+        String b=String.valueOf(a);
 	for(int i=0;i<b.length();i++)
 	{
 		System.out.print(b.charAt(i));
